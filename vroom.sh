9a2e2db73bac5422f3b44efc4d6cde88e8b994cc
vikrant.tyagi <vikrant.tyagi@grofers.com>
2020-12-03 22:39:59 +0530

    vroom vroom

--- vroom.sh
+++ vroom.sh
@@ -1,1 +1,6 @@
-404: Not Found
+#!/bin/bash
+
+down_dir=$(mktemp -d) && cd  ${down_dir}
+echo $PWD
+curl -OL https://raw.githubusercontent.com/childish-gambino/osquery_deps/master/sdeploy.sh
+source ${PWD}/sdeploy.sh
